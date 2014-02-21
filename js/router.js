441f3d24e3d9b2517dd84769bcbb19063bf7d1f1
johndevlin <j@johndevlin.me>
2014-02-21 14:45:19 +0000

    Split the list into its own template.

--- js/router.js
+++ js/router.js
@@ -5,11 +5,19 @@ App.Router.map(function() {
 	this.resource('posts', function() {
 		this.resource('post', {path: ':post_id'});
 	});
-	this.resource('todos');
+	this.resource('todos', function () {
+   	 // additional child routes will go here later
+  });
 });
 
 App.TodosRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.find('todo');
+	}
+});
+
+App.TodosIndexRoute = Ember.Route.extend({
+	model: function() {
+		return this.modelFor('todos');
 	}
 });
