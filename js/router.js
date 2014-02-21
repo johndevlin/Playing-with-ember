f48a38950ebc41a045ea2b1d0feb2a5fa2812100
johndevlin <j@johndevlin.me>
2014-02-21 16:31:21 +0000

    Balls. Only went and broke it.

--- js/router.js
+++ js/router.js
@@ -5,14 +5,19 @@ App.Router.map(function() {
 	this.resource('posts', function() {
 		this.resource('post', {path: ':post_id'});
 	});
-	this.resource('todos', function () {
-   	 // additional child routes will go here later
+	this.resource('todos', { path: 'todos' }, function () {
+		this.route('active');
   });
 });
 
 App.TodosRoute = Ember.Route.extend({
 	model: function() {
-		return this.store.find('todo');
+		return this.store.filter('todo', function(todo) {
+			return !todo.get('isCompleted');
+		});
+	},
+	renderTemplate: function(controller) {
+		this.render('todos/index', {controller: controller});
 	}
 });
 
