cc3f149419d5183e0fe029e364514e458a70b247
johndevlin <j@johndevlin.me>
2014-02-21 16:48:59 +0000

    Finished off the apps navigation.

--- js/router.js
+++ js/router.js
@@ -5,33 +5,49 @@ App.Router.map(function() {
 	this.resource('posts', function() {
 		this.resource('post', {path: ':post_id'});
 	});
-	this.resource('todos', function () {
-		
-		// additional child routes    
+	
+	this.resource('todos', function() { 
 		this.route('active');
-		
+		this.route('completed');
     });
 });
 
 App.TodosRoute = Ember.Route.extend({
-	model: function () {
+	model: function() {
 		return this.store.find('todo');
 	}
 });
 
 App.TodosIndexRoute = Ember.Route.extend({
-	model: function () {
+	model: function() {
 		return this.modelFor('todos');
 	}
 });
 
 App.TodosActiveRoute = Ember.Route.extend({
-	model: function(){
+	model: function() {
 		return this.store.filter('todo', function (todo) {
 			return !todo.get('isCompleted');
 		});
 	},
-	renderTemplate: function(controller){
-		this.render('todos/index', {controller: controller});
+	
+	renderTemplate: function(controller) {
+		this.render('todos/index', {
+			controller: controller
+		});
+	}
+});
+
+App.TodosCompletedRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.filter('todo', function(todo) {
+			return todo.get('isCompleted');
+		});
+	},
+	
+	renderTemplate: function(controller) {
+		this.render('todos/index', {
+			controller: controller
+		});
 	}
 });
