12bb718bb113b2ee8313b4ae879240e3357e3e1f
johndevlin <j@johndevlin.me>
2014-02-21 16:40:50 +0000

    And fixed it {{/balls}}.

--- js/router.js
+++ js/router.js
@@ -5,24 +5,33 @@ App.Router.map(function() {
 	this.resource('posts', function() {
 		this.resource('post', {path: ':post_id'});
 	});
-	this.resource('todos', { path: 'todos' }, function () {
+	this.resource('todos', function () {
+		
+		// additional child routes    
 		this.route('active');
-  });
+		
+    });
 });
 
 App.TodosRoute = Ember.Route.extend({
-	model: function() {
-		return this.store.filter('todo', function(todo) {
-			return !todo.get('isCompleted');
-		});
-	},
-	renderTemplate: function(controller) {
-		this.render('todos/index', {controller: controller});
+	model: function () {
+		return this.store.find('todo');
 	}
 });
 
 App.TodosIndexRoute = Ember.Route.extend({
-	model: function() {
+	model: function () {
 		return this.modelFor('todos');
+	}
+});
+
+App.TodosActiveRoute = Ember.Route.extend({
+	model: function(){
+		return this.store.filter('todo', function (todo) {
+			return !todo.get('isCompleted');
+		});
+	},
+	renderTemplate: function(controller){
+		this.render('todos/index', {controller: controller});
 	}
 });
