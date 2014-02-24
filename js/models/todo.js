502dc827eda6a1ac0855067d498fff7145fa9819
johndevlin <j@johndevlin.me>
2014-02-24 10:14:26 +0000

    Removed JSON data from the todo model.

--- js/models/todo.js
+++ js/models/todo.js
@@ -5,21 +5,3 @@ App.Todo = DS.Model.extend({
 	isCompleted: DS.attr('boolean')
 });
 
-// Todo app data
-App.Todo.FIXTURES = [
-	{
-		id: 1,
-		title: 'Create a Github repo',
-		isCompleted: true
-	},
-	{
-		id: 2,
-		title: 'Learn Ember',
-		isCompleted: false
-	},
-	{
-		id: 3,
-		title: 'Finish todo app',
-		isCompleted: false
-	}
-];
